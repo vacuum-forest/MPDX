14b531cd2103c406ce3460afaa32f1ef4cab5648
Vacuum Forest <40725146+vacuum-forest@users.noreply.github.com>
2019-03-23 23:19:22 -0400

    Yet more housekeeping...
    While I'm at it, why not?

---
+++ Maps/SC Stairwell A.lua
@@ -0,0 +1,9 @@
+Level.fog.color.r = 0
+Level.fog.color.g = 0
+Level.fog.color.b = 0
+Level.fog.depth = 20
+Level.fog.active = true
+Level.fog.affects_landscapes = false
+
+main_jump = loadfile("MPDX/Data/Lua/main.lua")
+main_jump()
