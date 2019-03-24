bcc5ad0d37063df0e03234cc67afdda005c8fc08
Kuu-Rin <Kuu-Rin@users.noreply.github.com>
2019-03-23 23:29:55 -0400

    Revert "Yet more housekeeping..."
    This reverts commit 14b531cd2103c406ce3460afaa32f1ef4cab5648.

--- Maps/Sewers A.lua
+++
@@ -1,9 +0,0 @@
-Level.fog.color.r = 0
-Level.fog.color.g = 0
-Level.fog.color.b = 0
-Level.fog.depth = 10
-Level.fog.active = true
-Level.fog.affects_landscapes = false
-
-main_jump = loadfile("MPDX/Data/Lua/main.lua")
-main_jump()
